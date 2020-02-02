78097bbb61820cbfc727857656b7f069631a5628
Daniel <danielfspencer1@gmail.com>
2020-02-02 18:36:11 +0000

    Improve modules

--- sound.js
+++ sound.js
@@ -1,42 +1,63 @@
 class Module {
   constructor() {
+    this.nodes = {}
+
     this.input = {}
     this.output = {}
+    this.tune = {}
   }
 }
 
 class Oscillator extends Module {
-  constructor(type, freq, detune) {
+  constructor(freq, detune) {
     super()
-    this.node = context.createOscillator()
-    this.node.type = type
-    this.node.frequency.value = freq
-    this.node.detune.value = detune
+
+    this.nodes.detune = context.createGain()
+    this.nodes.detune.gain.value = 6000
+
+    const types = ['sine', 'triangle', 'sawtooth', 'square']
+    for (let type of types) {
+      this.nodes[type] = context.createOscillator()
+      this.nodes[type].type = type
+      this.nodes[type].frequency.value = freq
+      this.nodes.detune.connect(this.nodes[type].detune)
+      this.nodes[type].start()
+    }
 
     this.input = {
-      freq_mod: this.node.detune
+      freq_mod: this.nodes.detune
     }
 
     this.output = {
-      sine: {connect: (dest) => {this.node.connect(dest)}}
+      sin: this.nodes.sine,
+      tri: this.nodes.triangle,
+      saw: this.nodes.sawtooth,
+      sqr: this.nodes.square
     }
-    this.node.start()
+
+    this.tune = {
+      set_freq: (freq) => {
+        for (let type of types) {
+          this.nodes[type].frequency.value = freq
+        }
+      }
+    }
   }
 }
 
 class Amplifer extends Module {
   constructor(gain) {
     super()
-    this.node = context.createGain()
-    this.node.gain.value = gain
+    this.nodes.gain = context.createGain()
+    this.nodes.gain.gain.value = gain
 
     this.input = {
-      signal: this.node,
-      gain: this.node.gain
+      signal: this.nodes.gain,
+      gain: this.nodes.gain.gain
     }
 
     this.output = {
-      signal: {connect: (dest) => {this.node.connect(dest)}}
+      signal: this.nodes.gain
     }
   }
 }
@@ -55,25 +76,23 @@ function init () {
   context = new AudioContext()
 
   let out = new Output()
-  let sin = new Oscillator("sine", 330, 0)
-  let gain = new Amplifer(0.5)
 
-  let gain2 = new Amplifer(100)
+  let main = new Oscillator(330)
 
-  let mod = new Oscillator("saw", 2, 0)
+  let lfo1 = new Oscillator(0.5)
+  let lfo2 = new Oscillator(8)
 
-  mod.output.sine.connect(gain.input.gain)
-  mod.output.sine.connect(gain2.input.signal)
+  let gain1 = new Amplifer(0.1)
+  let gain2 = new Amplifer(0.3)
 
-  gain2.output.signal.connect(sin.input.freq_mod)
+  let out_gain = new Amplifer(0.3)
 
-  sin.output.sine.connect(gain.input.signal)
-  gain.output.signal.connect(out.input.signal)
+  lfo1.output.sin.connect(gain1.input.signal)
+  gain1.output.signal.connect(lfo2.input.freq_mod)
 
+  lfo2.output.saw.connect(gain2.input.signal)
+  gain2.output.signal.connect(main.input.freq_mod)
 
-  // osctest = new osc("sine",400,0);
-  // osctest.play(context.destination , 1);
-  //
-  // osctest2 = new osc("sine", 10, 0);
-  // osctest2.play(osctest.oscillator.detune , 100);
+  main.output.sin.connect(out_gain.input.signal)
+  out_gain.output.signal.connect(out.input.signal)
 }
