846f9f3116e211293771ed4adcb09e7306875f3f
Daniel <danielfspencer1@gmail.com>
2020-04-03 20:43:20 +0100

    Fix initial slider positions

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -6,9 +6,9 @@ const LOG_SLIDER_MAX = 10000
 function normalToLog (value, min, max) {
   const minValue = Math.log(min)
   const maxValue = Math.log(max)
-  const scale = (minValue - maxValue) / (LOG_SLIDER_MAX - LOG_SLIDER_MIN)
+  const scale = (maxValue - minValue) / (LOG_SLIDER_MAX - LOG_SLIDER_MIN)
 
-  return (Math.log(value) - min / scale + LOG_SLIDER_MIN)
+  return LOG_SLIDER_MIN + (Math.log(value) - minValue) / scale
 }
 
 function logToNormal (position, min, max) {
